3e731a596cf85285baff84e4acc746f8545bcfa7
elombe <elombe@swftstudios.com>
2026-03-24 14:48:35 -0400

    Initial Commit

---
+++ Velocity/VelocityApp.swift
@@ -0,0 +1,17 @@
+//
+//  VelocityApp.swift
+//  Velocity
+//
+//  Created by Elombe.Kisala on 3/24/26.
+//
+
+import SwiftUI
+
+@main
+struct VelocityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
